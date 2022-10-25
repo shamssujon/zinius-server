61a223f338baa67e290308e38a7b02507d5405f4
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-10-25 16:57:18 +0600

    Use cors

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.send("Zinius server is running...");
 });
