7ede7d60702e141507403e2827c2b9d3d0eaf711
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-10-27 16:49:41 +0600

    Single course category api

--- index.js
+++ index.js
@@ -9,20 +9,30 @@ app.get("/", (req, res) => {
     res.send("Zinius server is running...");
 });
 
+// Categories
 const categories = require("./data/categories.json");
 app.get("/categories", (req, res) => {
     res.send(categories);
 });
 
+// Courses
 const courses = require("./data/courses.json");
 app.get("/courses", (req, res) => {
     res.send(courses);
 });
 
+// Single course
 app.get("/course/:id", (req, res) => {
     const id = req.params.id;
-    const singleCourse = courses.find(course => course.id === id);
-    res.send(singleCourse)
+    const singleCourse = courses.find((course) => course.id === id);
+    res.send(singleCourse);
+});
+
+// Single Course Category
+app.get("/category/:id", (req, res) => {
+    const id = req.params.id;
+    const singleCategory = courses.filter((category) => category.category_id === id);
+    res.send(singleCategory);
 });
 
 app.listen(port, () => {
